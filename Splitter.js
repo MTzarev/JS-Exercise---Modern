03f27a66fa7f6cac6f399cfd5d65cf7fb7384c1a
Miroslav Tsarev <mtzarev@gmail.com>
2024-09-21 23:49:43 +0300

    Add files via upload

---
+++ Splitter.js
@@ -0,0 +1,16 @@
+function newPascalCaseSplitter(str){
+ newArr = [];
+ let idx =0
+ for (let i = 1; i<str.length; i++){
+    if (str[i]===str[i].toUpperCase()){
+        let newWord = str.slice(idx, i);
+        newArr.push(newWord);
+        idx = i
+    }
+ }
+ newArr.push(str.slice(idx))
+ 
+    console.log(newArr.join(`; `));
+    
+}
+newPascalCaseSplitter(`SplitMeIfYouCanHaHaYouCantOrYouCan`);

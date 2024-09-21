6c162e3977898926cb7c21754f896a627991c349
Miroslav Tsarev <mtzarev@gmail.com>
2024-09-21 23:50:21 +0300

    Update Splitter.js

--- Splitter.js
+++ Splitter.js
@@ -1,16 +1,14 @@
 function newPascalCaseSplitter(str){
  newArr = [];
- let idx =0
+ let idx =0;
  for (let i = 1; i<str.length; i++){
     if (str[i]===str[i].toUpperCase()){
         let newWord = str.slice(idx, i);
         newArr.push(newWord);
-        idx = i
+        idx = i;
     }
  }
- newArr.push(str.slice(idx))
- 
+ newArr.push(str.slice(idx));
     console.log(newArr.join(`; `));
-    
 }
 newPascalCaseSplitter(`SplitMeIfYouCanHaHaYouCantOrYouCan`);
